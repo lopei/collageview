5614335e54479f52b9a6714e52aefa47d68917bc
Alan <alan.matvienko@mobindustry.net>
2016-12-08 14:16:09 +0200

    0.0.4 sample

--- app/src/main/java/com/lopei/myapplication/MainActivity.java
+++ app/src/main/java/com/lopei/myapplication/MainActivity.java
@@ -1,13 +1,8 @@
 package com.lopei.myapplication;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.lopei.collageview.CollageView;
 
@@ -18,12 +13,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         CollageView collageView = (CollageView) findViewById(R.id.collageView);
 
         collageView
                 .photoMargin(1)
                 .useSquarePhotos(true)
                 .placeHolder(R.drawable.placeholder_photo)
-                .loadPhotos(getResources().getStringArray(R.array.urls));
+                .loadPhotos(getDrawablesIds());
+    }
+
+    private int[] getDrawablesIds() {
+        TypedArray imgs = getResources().obtainTypedArray(R.array.resids);
+        int[] ids = new int[imgs.length()];
+        for (int i = 0; i < imgs.length(); i++) {
+            ids[i] = imgs.getResourceId(i, -1);
+        }
+        imgs.recycle();
+        return ids;
+    }
+
+    private String[] getUrls() {
+        return getResources().getStringArray(R.array.urls);
     }
 }
